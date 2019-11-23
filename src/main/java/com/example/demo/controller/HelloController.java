102e817d1ed7ec787b2620840697c4c9c2936014
xuchao <xuchao@bakweilai.com>
2019-11-23 14:16:41 +0800

    提交jenkins项目

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description TODO
+ * @Author xu
+ * @Date 2019/11/23 14:11
+ **/
+@RestController
+public class HelloController {
+
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello jenkins";
+    }
+}
